438b018ca8eee9ab63c3f47b8c6bdb7cfa010157
kiwenlau <kiwenlau@126.com>
2015-05-14 18:39:46 +0900

    test prorgram

---
+++ test/test.c
@@ -0,0 +1,14 @@
+// printf the length of muticharacter
+
+# include<stdio.h>
+# include<stdlib.h>
+# include<string.h>
+
+int main()
+{
+    printf("%d\n", strlen("│"));
+    
+    printf("%d\n", strlen("├─"));
+}
+
+
