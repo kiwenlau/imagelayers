57b18d2d636a84847fd3667a567526ffb89bde2d
kiwenlau <kiwenlau@126.com>
2015-05-14 13:02:29 +0900

    tune output form

--- imagelayers.c
+++ imagelayers.c
@@ -1,4 +1,6 @@
-// calculate image layers base on the output of "docker images layers"
+// calculate how many layers of all docker images
+// it is base on the output of "docker images -tree"
+// unfortunately '-tree' is deprecated, it will be removed soon
 
 # include<stdio.h>
 # include<stdlib.h>
@@ -7,32 +9,37 @@
 int main()
 {
     FILE *fp;
-    char str[200], *substr,*s;
-    int len, index;
+    char str[200], *substr, *images;
+    int len, N, layers;
     
-    system("docker images -tree > tempfile1.txt");
+    system("docker images -tree > tempfile1.txt 2>&1");
     system("cat tempfile1.txt | grep Tags > tempfile2.txt");
     fp=fopen("tempfile2.txt", "rt");
     
-    printf("\n");
+    printf("LAYERS   IMAGE\n");
     while(fgets(str, 200, fp))
     {
-      
+        //replace '\n' with '\0'
         len=strlen(str);
         str[len-1]='\0';
+        
+        //caculate the number of image layers
         substr=strstr(str, "Virtual");
-        index=strlen(str)-strlen(substr);
+        N=strlen(str)-strlen(substr);
         if (!strstr(str, "│"))
-            index=index+2;
-        substr=strstr(str, "Tags");
-        printf("image layers=%2d:  %s\n", (index-19)/2, substr+6);
+            N=N+2;
+        layers=(N-19)/2;
+        
+        //get the image name
+        images=strstr(str, "Tags");
+        images=images+6;
+        
+        printf("%2d:      %s\n", layers, images);
     }
-    printf("\n");
     
     remove("tempfile1.txt");
     remove("tempfile2.txt");
     
-
     fclose(fp);
 }
 
